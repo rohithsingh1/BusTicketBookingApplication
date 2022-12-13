bc3cd3b74c08a335e0b9a4c193ec42c0d7fe2646
rohithsingh1 <rohithsinghthakur3@gmail.com>
2022-12-13 21:36:28 +0530

    second commit for deployment other than heroku

--- server.js
+++ server.js
@@ -18,15 +18,20 @@ app.use('/api/users',usersRoute)
 app.use('/api/buses',busesRoute)
 app.use('/api/bookings',bookingsRoute)
 
-const path = require("path");
-if(process.env.NODE_ENV === "production")
-{
-    app.use(express.static("client/build"));
+const path=require("path");
+app.use(express.static("client/build"));
   
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
     });
-}
+// if(process.env.NODE_ENV === "production")
+// {
+//     app.use(express.static("client/build"));
+  
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+//     });
+// }
 
 
 const port = process.env.PORT || 5000
