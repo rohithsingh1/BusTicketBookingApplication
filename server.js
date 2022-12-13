82977ed20f2f725904a7132aa2fcca000772d25c
rohithsingh1 <rohithsinghthakur3@gmail.com>
2022-12-13 22:13:32 +0530

    third commit for deployment other than heroku

--- server.js
+++ server.js
@@ -19,11 +19,12 @@ app.use('/api/buses',busesRoute)
 app.use('/api/bookings',bookingsRoute)
 
 const path=require("path");
-app.use(express.static("client/build"));
-  
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-    });
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
 // if(process.env.NODE_ENV === "production")
 // {
 //     app.use(express.static("client/build"));
