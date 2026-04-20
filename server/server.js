d106981da56dd4131bdab9e19b61375dabbaf466
Thakur Rohith <thakurrohith@Thakurs-MacBook-Air.local>
2026-04-20 14:34:04 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Bus Ticket Booking API is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
