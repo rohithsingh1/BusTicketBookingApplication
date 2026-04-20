2ab4781e822819ed86b91b96e162c9917f5aa5ff
Thakur Rohith <thakurrohith@Thakurs-MacBook-Air.local>
2026-04-20 14:56:52 +0530

    seting up the initial server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
@@ -14,6 +15,23 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const server = app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+server.on("error", (error) => {
+  console.error("Server failed to start:", error);
+});
+
+server.on("close", () => {
+  console.log("HTTP server was closed.");
+});
+
+["SIGINT", "SIGTERM"].forEach((signal) => {
+  process.on(signal, () => {
+    console.log(`Received ${signal}. Shutting down server...`);
+    server.close(() => {
+      process.exit(0);
+    });
+  });
 });
